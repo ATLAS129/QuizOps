cca3445425ab41d731719f9a6d0299a5a169119f
ATLAS129 <yanis20092525@gmail.com>
2026-07-12 22:16:20 +0300

    chore: added configuration in nestjs

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './modules/prisma/prisma.module.js';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+  ],
 })
 export class AppModule {}
