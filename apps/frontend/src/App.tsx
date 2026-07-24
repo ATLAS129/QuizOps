bfa89623962b892ea62b5eeeeeb7dd7d0310d25c
ATLAS129 <yanis20092525@gmail.com>
2026-07-24 22:20:38 +0300

    feat: added routing

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,5 +1,7 @@
+import AppRoutes from "./routes/AppRoutes";
+
 function App() {
-  return <div className=""></div>;
+  return <AppRoutes />;
 }
 
 export default App;
---
+++ apps/frontend/src/routes/AppRoutes.tsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from "react-router";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<div>Main page</div>} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
