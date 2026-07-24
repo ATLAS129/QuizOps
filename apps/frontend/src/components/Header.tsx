3e31d784c18a3451c12f9e6cfde38038c2480ac5
ATLAS129 <yanis20092525@gmail.com>
2026-07-24 22:52:15 +0300

    feat: added initial header

---
+++ apps/frontend/src/components/Header.tsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router";
+
+const Header = () => {
+  return (
+    <header className="h-14 w-full bg-bg-surface flex items-center justify-between px-6">
+      <div>
+        <h1>Logo QuizOps</h1>
+      </div>
+
+      <div className="flex items-center justify-center gap-6 text-sm">
+        <Link to={"/profile"}>All my decks</Link>
+        <div className="size-8 bg-purple-400 rounded-full flex justify-center items-center text-sm select-none cursor-pointer">
+          ME
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- apps/frontend/src/layouts/MainLayout.tsx
+++ apps/frontend/src/layouts/MainLayout.tsx
@@ -1,8 +1,11 @@
 import { Outlet } from "react-router";
+import Header from "../components/Header";
 
 const MainLayout = () => {
   return (
     <>
+      <Header />
+
       <main className="container mx-auto min-h-screen">
         <Outlet />
       </main>
