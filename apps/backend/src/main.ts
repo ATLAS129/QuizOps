18b5349122a97973d924597036e45d196aacb1fb
ATLAS129 <yanis20092525@gmail.com>
2026-09-19 19:20:55 +0300

    chore: updated cors origin

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -6,14 +6,18 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const configService = app.get(ConfigService);
+
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin:
+      configService.get('NODE_ENV') == 'production'
+        ? 'https://quiz-ops-frontend.vercel.app/'
+        : 'http://localhost:5173',
     credentials: true,
   });
 
   app.use(cookieParser());
 
-  const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT', 3000);
 
   await app.listen(port);
