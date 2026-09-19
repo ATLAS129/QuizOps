6686e547bbf67a03d4d1e89ffd8e6962da8d4ad6
ATLAS129 <yanis20092525@gmail.com>
2026-09-19 19:26:09 +0300

    chore: updated url in cors origin

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.enableCors({
     origin:
       configService.get('NODE_ENV') == 'production'
-        ? 'https://quiz-ops-frontend.vercel.app/'
+        ? 'https://quiz-ops-frontend.vercel.app'
         : 'http://localhost:5173',
     credentials: true,
   });
